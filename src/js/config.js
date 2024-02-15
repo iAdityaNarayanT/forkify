626fce94a52548294597d5908705dee8d02e3651
Aditya Narayan Tiwari <vipraaditya@gmail.com>
2024-02-15 23:51:13 +0530

    update: api key

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'dce698d7-18e7-4ac9-9cb4-147780bf543b';
+export const MODAL_CLOSE_SEC = 2.5;
